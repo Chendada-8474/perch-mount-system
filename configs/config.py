10a70a07d4ef7de4922b7b65ca531a13e9dec7f3
chendada-8474 <raymond96383@gmail.com>
2023-08-18 09:55:32 +0800

    config

---
+++ configs/config.py
@@ -0,0 +1,14 @@
+HOST = "http://127.0.0.1:5000"
+EMPTY_CHECK_LIMIT = 250
+REVIEW_LIMIT = 100
+NUM_MEDIA_IN_PAGE = 50
+
+IMAGE_EXTENSIONS = {"bmp", "jpg", "jpeg", "png", "tif", "tiff", "dng"}
+VIDEO_EXTENSIONS = {"mov", "avi", "mp4", "mpg", "mpeg", "m4v", "wmv", "mkv"}
+
+RAPTOR_ORDERS = {"Strigiformes", "Accipitriformes"}
+
+cache = {
+    "DEBUG": True,  # some Flask specific configs
+    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
+}
