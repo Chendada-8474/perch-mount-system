53ffb98fa23de345155e420fd2c4926b6cb595a0
chendada-8474 <raymond96383@gmail.com>
2023-09-22 00:39:20 +0800

    fixed create perch mount error when no layer

--- static/js/api/post_perch_mount.js
+++ static/js/api/post_perch_mount.js
@@ -29,12 +29,15 @@ window.addEventListener('load', function () {
 })
 
 function getPerchMountData() {
+
+    var layer = (!layer) ? null : layer;
+
     return {
         "perch_mount_name": perchMountNameInput.value,
         "latitude": latitudeInput.value,
         "longitude": longitudeInput.value,
         "project": projectSelect.value,
         "habitat": habitatSelect.value,
-        "layer": layerSelect.value,
+        "layer": layer,
     };
 }
