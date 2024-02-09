5206beb9346f1eb539a3058f67e90aefe31d3ec4
Chendada-8474 <raymond96383@gmail.com>
2024-02-09 13:37:56 +0800

    refactor: use session maker to init session in __init__

--- service/__init__.py
+++ service/__init__.py
@@ -1,19 +1,24 @@
-from src.config import MySQL as ConfMySQL
-from sqlalchemy import create_engine
+import sqlalchemy
+import sqlalchemy.orm
+
+import src.config as config
 
 DATABASE_NAME = "perch_mount"
 
 SQLALCHEMY_DATABASE_URI = "mysql+pymysql://%s:%s@%s:%s/%s" % (
-    ConfMySQL.get_mysql_user(),
-    ConfMySQL.get_mysql_password(),
-    ConfMySQL.get_mysql_host(),
-    ConfMySQL.get_mysql_port(),
+    config.MySQL.get_mysql_user(),
+    config.MySQL.get_mysql_password(),
+    config.MySQL.get_mysql_host(),
+    config.MySQL.get_mysql_port(),
     DATABASE_NAME,
 )
 
-db_engine = create_engine(
+db_engine = sqlalchemy.create_engine(
     SQLALCHEMY_DATABASE_URI,
     pool_recycle=3600,
     pool_pre_ping=True,
     # isolation_level="AUTOCOMMIT",
 )
+
+
+session = sqlalchemy.orm.sessionmaker(db_engine)
