0de38f51433e3b384fc1f2e6f209e90069c7dc60
Chendada-8474 <raymond96383@gmail.com>
2024-02-10 17:40:11 +0800

    routing

--- src/api.py
+++ src/api.py
@@ -1,5 +1,49 @@
 import flask_restful
-from resources import *
+from resources import perch_mounts
+from resources import sections
+from resources import empty_media
+from resources import detected_media
+from resources import media
+
+
+# ROUTES = [
+#     {
+#         # perch_mounts
+#         "route": "perch_mounts",
+#         "resources": [perch_mounts.PerchMounts, perch_mounts.PerchMount],
+#         "children": [
+#             {
+#                 "route": "<int:perch_mount_id>",
+#                 "resource": perch_mounts.PerchMount,
+#                 "children": [],
+#             }
+#         ],
+#     },
+#     {
+#         # sections
+#         "route": "sections",
+#         "resources": [sections.Section, sections.Sections],
+#         "children": [
+#             {
+#                 "route": "<int:section_id>",
+#                 "resources": [sections.Section],
+#                 "children": [],
+#             }
+#         ],
+#     },
+# ]
+
+
+# class PerchMountApi(flask_restful.Api):
+#     def add_resources(self, routes: list[dict], parent: str = "/"):
+#         for route in routes:
+
+#             for resource in route["resources"]:
+#                 self.add_resource(resource, route["route"])
+
+#             parent = f"{parent}{route}/"
+#             self.add_resources(route["children"], parent=parent)
+
 
 api = flask_restful.Api()
 
