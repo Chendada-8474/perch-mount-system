8784b181b71177ab668a5450b207ea39719fef63
Tatsutomo <raymond96383@gmail.com>
2024-02-14 13:53:44 +0800

    refine]

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ from src import config
 from login import authenticate
 
 app = flask.Flask(__name__)
-app.config["SECRET_KEY"] = config.get_file_content(config.EnvKeys.FLASK_SECRET)
 app.config["SQLALCHEMY_DATABASE_URI"] = service.SQLALCHEMY_DATABASE_URI
-app.config["JWT_SECRET_KEY"] = config.EnvKeys.JWT_SECRET
+app.config["SECRET_KEY"] = config.get_file_content(config.EnvKeys.FLASK_SECRET)
+app.config["JWT_SECRET_KEY"] = config.get_file_content(config.EnvKeys.JWT_SECRET)
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = config.get_jwt_access_token_expires()
 
 api = route_helper.PerchMountApi(app)
